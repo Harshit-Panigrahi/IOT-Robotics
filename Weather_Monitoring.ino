81c68a83fa2cbe4e02d893447a03fc1ea6f929a2
Harshit Panigrahi <61592605+Harshit-Panigrahi@users.noreply.github.com>
2023-04-06 17:55:24 +0530

    Add files via upload

--- Weather_Monitoring.ino
+++ Weather_Monitoring.ino
@@ -1,58 +1,86 @@
-#include <WiFi.h>
-#include <WiFiClient.h>
+#include <Wire.h>
 #include <Adafruit_BMP085.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 #include "DHT.h"
-
-#define DHT_PIN 21
-#define DHT_TYPE DHT11
-DHT dht(DHT_PIN, DHT_TYPE);
+#define DHTPIN 19
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
 #define WLAN_SSID "virusnet"
 #define WLAN_PASS "air@6999"
 
 Adafruit_BMP085 bmp;
+String serverName = "http://192.168.1.13:5000/add-data";
 
 void setup() {
   Serial.begin(9600);
-  
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
   WiFi.begin(WLAN_SSID, WLAN_PASS);
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print("*");
     delay(500);
+    Serial.print(".");
   }
-  Serial.println("\nWifi has been connected.");
-  Serial.print("IP Addr: ");
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
   if (!bmp.begin()) {
-    Serial.println("BMP180 Sensor not found");
+    Serial.println("Could not find a valid BMP085/BMP180 sensor, check wiring!");
     while (1) {}
   }
-
   dht.begin();
 }
 
 void loop() {
   if (WiFi.status() == WL_CONNECTED) {
-    WiFiClient cli;
-    p = bmp.readPressure();
-    t = bmp.readTemperature();
-    a = bmp.readAltitude();
-    Serial.print("\nPressure: ");
-    Serial.println(p);
-    Serial.print("Temperature: ");
-    Serial.println(t);
-    Serial.print("Altitude: ");
-    Serial.println(a);
-    
-    float h = bmp.readHumidity();
+    WiFiClient client;
+    HTTPClient http;
+
+    Serial.print("Temperature = ");
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print("Pressure = ");
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    Serial.print("Altitude = ");
+    Serial.print(bmp.readAltitude());
+    Serial.println(" meters");
+
+    Serial.print("Pressure at sealevel (calculated) = ");
+    Serial.print(bmp.readSealevelPressure());
+    Serial.println(" Pa");
+
+    Serial.print("Real altitude = ");
+    Serial.print(bmp.readAltitude(102000));
+    Serial.println(" meters");
+    Serial.println();
+
+    float h = dht.readHumidity();
     if (isnan(h)) {
-      Serial.print("Failed to read himidity");    
+      Serial.println("Failed to read from DHT sensor!");
       return;
-    } else {
-      Serial.print("Humidity: ");
-      Serial.println(h);
     }
+    else {
+      Serial.print("Humidity: ");
+      Serial.print(h);
+    }
+
+    http.begin(serverName);
+    http.addHeader("Content-Type", "application/json");
+    
+    int httpResponseCode = http.POST("{\"temperature\" : " + String(bmp.readTemperature()) + ",\"altitude\" : " + String(bmp.readAltitude()) + ",\"pressure\" : " + String(bmp.readAltitude(102000)) + ",\"humidity\" : " + String(dht.readHumidity()) + "}");
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    http.end();
   }
-  else Serial.println("WiFi disconnected");
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+  delay(3000);
 }
